936b50d9f6f5a8dccd68200f0f984d6d3f953339
Jacob Bloom <mr.jacob.bloom@gmail.com>
2017-07-14 21:51:56 -0600

    initial commit, mostly boilerplate before I realized that I had to change the name lol

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # music
+
+A lead-sheet editor and chord player a la iReal (which you should buy because it's far more useful)
---
+++ src/editor.js
@@ -0,0 +1,10 @@
+/*
+ * Code for the editor, but nothing specific to running it on the webpage.
+ */
+
+/* MusicEditor object, which houses everything else
+ * @class
+ */
+function MusicEditor() {
+  this.container = document.createElement('div');
+}
